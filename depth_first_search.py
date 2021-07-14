afed980cd4db1c0160991f929c6c83ec683764d6
Jade Nguyen <75640141+jadenguyen0128@users.noreply.github.com>
2021-07-14 08:15:43 -0700

    add comment for resources

--- depth_first_search.py
+++ depth_first_search.py
@@ -1,3 +1,15 @@
+# depth first search
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+# inspired by kenny's tutorial: https://www.youtube.com/watch?v=nNGSZdx6F3M&t=305s
+
+# similar problems with letter combinations from phone number
+# generate parentheses
+# permutation
+# combination sum
+# subsets
+
+
 def letter_combinations(digits):
     if len(digit) == 0:
         return []
